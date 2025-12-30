4dd7fd8e65b157c06c555d624d3e1ca557095407
veena19nayak@gmail.com <veena19nayak@gmail.com>
2025-12-30 06:46:26 +0530

    calculation

--- calculate.py
+++ calculate.py
@@ -4,3 +4,8 @@ num2 = float(input("Enter second number: "))
 print("Sum:", num1 + num2)
 print("Difference:", num1 - num2)
 print("Product:", num1 * num2)
+
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division not possible (division by zero)")
